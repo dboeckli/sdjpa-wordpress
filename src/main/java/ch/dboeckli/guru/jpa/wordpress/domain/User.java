6ba0c1957dbac5572a386acc01f9438f837152ee
adm-boeckli <dboeckli@gmail.com>
2025-04-15 13:24:57 +0200

    add user table

---
+++ src/main/java/ch/dboeckli/guru/jpa/wordpress/domain/User.java
@@ -0,0 +1,46 @@
+package ch.dboeckli.guru.jpa.wordpress.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "wp_users")
+@Getter
+@Setter
+@ToString
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_login")
+    private String login;
+
+    @Column(name = "user_pass")
+    private String password;
+
+    @Column(name = "user_nicename")
+    private String nicename;
+
+    @Column(name = "user_email")
+    private String email;
+
+    @Column(name = "user_url")
+    private String url;
+
+    @Column(name = "user_registered")
+    private Timestamp registered;
+
+    @Column(name = "user_activation_key")
+    private String activationKey;
+
+    @Column(name = "user_status")
+    private Integer status;
+
+    @Column(name = "display_name")
+    private String displayName;
+}
